55ff1d93f99c31dca295873f97eb17e80973d61c
Stuart Shay <sshay@yahoo.com>
2022-03-15 20:24:50 -0400

    Develop (#4)
    * Update Github Actions
    
    * Update
    
    * Update Github Actions
    
    * Update Github Actions
    
    * Update Github Actions
    
    * Update
    
    * Update Docker
    
    * Update Docker
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Add InfluxDb
    
    * Add InfluxDb

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AzureConnectedServices
+
+[![Deploy Azure Linux Container](https://github.com/stuartshay/AzureConnectedServices/actions/workflows/deploy-azure-container.yml/badge.svg)](https://github.com/stuartshay/AzureConnectedServices/actions/workflows/deploy-azure-container.yml)
--- src/AzureConnectedServices/Program.cs
+++ src/AzureConnectedServices/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 var services = builder.Services;
 
-SetupConfiguration();
+//SetupConfiguration();
 SetupServices();
 AddServices();
 
@@ -17,11 +17,11 @@ app.Run();
 void SetupConfiguration()
 {
     // Add Azure App Configuration to the container.
-    var azAppConfigConnection = builder.Configuration["AppConfig"];
+    var azAppConfigConnection = configuration["AppConfig"];
     if (!string.IsNullOrEmpty(azAppConfigConnection))
     {
         // Use the connection string if it is available.
-        builder.Configuration.AddAzureAppConfiguration(options =>
+        configuration.AddAzureAppConfiguration(options =>
         {
             options.Connect(azAppConfigConnection)
                 .ConfigureRefresh(refresh =>
@@ -31,12 +31,12 @@ void SetupConfiguration()
                 });
         });
     }
-    else if (Uri.TryCreate(builder.Configuration["Endpoints:AppConfig"], UriKind.Absolute, out var endpoint))
+    else if (Uri.TryCreate(configuration["Endpoints:AppConfig"], UriKind.Absolute, out var endpoint))
     {
         // Use Azure Active Directory authentication.
         // The identity of this app should be assigned 'App Configuration Data Reader' or 'App Configuration Data Owner' role in App Configuration.
         // For more information, please visit https://aka.ms/vs/azure-app-configuration/concept-enable-rbac
-        builder.Configuration.AddAzureAppConfiguration(options =>
+        configuration.AddAzureAppConfiguration(options =>
         {
             options.Connect(endpoint, new DefaultAzureCredential())
                 .ConfigureRefresh(refresh =>
@@ -46,14 +46,15 @@ void SetupConfiguration()
                 });
         });
     }
+
     builder.Services.AddAzureAppConfiguration();
 }
 
 void SetupServices()
 {
-    builder.Services.AddControllers();
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    services.AddControllers();
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
 }
 
 void AddServices()
@@ -63,14 +64,15 @@ void AddServices()
 
 void SetupApp()
 {
-    if (app.Environment.IsDevelopment())
-    {
+    //if (app.Environment.IsDevelopment())
+    //{
         app.UseSwagger();
         app.UseSwaggerUI();
-    }
-    app.UseAzureAppConfiguration();
+    //}
+    
+    //app.UseAzureAppConfiguration();
 
-    app.UseHttpsRedirection();
+    //app.UseHttpsRedirection();
 
     app.UseAuthorization();
 
