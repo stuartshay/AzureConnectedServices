17c454376031908a669b7c15b23ecfb4a1aad7e8
Stuart Shay <sshay@yahoo.com>
2022-03-17 00:26:56 -0400

    Update

---
+++ src/AzureConnectedServices.WorkerService/Program.cs
@@ -0,0 +1,10 @@
+using AzureConnectedServices.WorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
