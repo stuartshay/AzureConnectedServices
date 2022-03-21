d29eb751b9e8661c4cb9887eb64ff707b5013423
Stuart Shay <sshay@yahoo.com>
2022-03-21 19:30:11 -0400

    Update

--- src/AzureConnectedServices.WorkerService/Program.cs
+++ src/AzureConnectedServices.WorkerService/Program.cs
@@ -1,13 +1,23 @@
 using AzureConnectedServices.WorkerService;
+using TinyHealthCheck;
+using TinyHealthCheck.HealthChecks;
+using TinyHealthCheck.Models;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
+        services.AddBasicTinyHealthCheck(config =>
+        {
+            config.Port = 3901;
+            config.Hostname = "*";
+            return config;
+        });
     })
     .ConfigureAppConfiguration((hostingContext, config) =>
     {
-
+        //https://www.johanohlin.com/posts/2019-10-22-using-azure-app-config-in-a-worker-service/
+        //https://docs.microsoft.com/en-us/azure/azure-app-configuration/quickstart-dotnet-core-app
     })
     .Build();
 
