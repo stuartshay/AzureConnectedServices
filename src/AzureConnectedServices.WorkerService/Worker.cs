65abf1c6d3df057268d37c9c831544f3f075afbc
Stuart Shay <sshay@yahoo.com>
2022-03-24 23:56:55 -0400

    Add Service Bus

--- src/AzureConnectedServices.WorkerService/Worker.cs
+++ src/AzureConnectedServices.WorkerService/Worker.cs
@@ -1,19 +1,16 @@
-using AzureConnectedServices.Core.Configuration;
-using Microsoft.Extensions.Options;
-
 namespace AzureConnectedServices.WorkerService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
         private readonly WorkerStateService _workerStateService;
-        private readonly Settings _settings;
+        private readonly IConfiguration _configuration;
 
-        public Worker(ILogger<Worker> logger, WorkerStateService workerStateService, IOptions<Settings> settings)
+        public Worker(ILogger<Worker> logger, WorkerStateService workerStateService, IConfiguration configuration)
         {
             _logger = logger;
             _workerStateService = workerStateService;
-            _settings = settings.Value;
+            _configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -21,9 +18,12 @@ namespace AzureConnectedServices.WorkerService
             while (!stoppingToken.IsCancellationRequested)
             {
                 _workerStateService.IsRunning = true;
-                
+
+                string key = "AzureConnectedServices:Settings:ServiceBusConnectionString";
+                string message = _configuration[key];
+
                 _logger.LogInformation("Worker iteration {0} running at: {time}", _workerStateService.Iteration, DateTimeOffset.Now);
-                _logger.LogInformation("ServiceBusConnectionString {0}", _settings?.ServiceBusConnectionString);
+                _logger.LogInformation("ServiceBusConnectionString {0}", message);
 
                 await Task.Delay(2500, stoppingToken);
                 _workerStateService.Iteration++;
