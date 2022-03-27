935e281cd15a8b9c4fd2540d0b5db98d71d7b0c0
Stuart Shay <sshay@yahoo.com>
2022-03-26 23:21:11 -0400

    Update

--- src/AzureConnectedServices.WorkerService/Worker.cs
+++ src/AzureConnectedServices.WorkerService/Worker.cs
@@ -1,3 +1,7 @@
+using Azure.Messaging.ServiceBus;
+using AzureConnectedServices.Models;
+using System.Text.Json;
+
 namespace AzureConnectedServices.WorkerService
 {
     public class Worker : BackgroundService
@@ -6,11 +10,19 @@ namespace AzureConnectedServices.WorkerService
         private readonly WorkerStateService _workerStateService;
         private readonly IConfiguration _configuration;
 
+        private ServiceBusClient _client;
+        private ServiceBusProcessor _processor;
+
         public Worker(ILogger<Worker> logger, WorkerStateService workerStateService, IConfiguration configuration)
         {
             _logger = logger;
             _workerStateService = workerStateService;
             _configuration = configuration;
+
+            string key = "AzureConnectedServices:Settings:ServiceBusConnectionString";
+            string connectionString = _configuration[key];
+
+            _client = new ServiceBusClient(connectionString);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -20,15 +32,51 @@ namespace AzureConnectedServices.WorkerService
                 _workerStateService.IsRunning = true;
 
                 string key = "AzureConnectedServices:Settings:ServiceBusConnectionString";
-                string message = _configuration[key];
+                string connectionString = _configuration[key];
 
                 _logger.LogInformation("Worker iteration {0} running at: {time}", _workerStateService.Iteration, DateTimeOffset.Now);
-                _logger.LogInformation("ServiceBusConnectionString {0}", message);
+                _logger.LogInformation("ServiceBusConnectionString {0}", connectionString);
 
-                await Task.Delay(2500, stoppingToken);
-                _workerStateService.Iteration++;
+                _processor = _client.CreateProcessor("weatherrequest", new ServiceBusProcessorOptions());
+                _processor.ProcessMessageAsync += MessageHandler;
+                _processor.ProcessErrorAsync += ErrorHandler;
+
+                 await _processor.StartProcessingAsync();
+
+                 await Task.Delay(2500, stoppingToken);
+                 _workerStateService.Iteration++;
             }
-            _workerStateService.IsRunning = false;
+
+          _workerStateService.IsRunning = false;
         }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _processor.StopProcessingAsync();
+            await base.StopAsync(cancellationToken);
+        }
+
+        public override void Dispose()
+        {
+            _processor.DisposeAsync().GetAwaiter().GetResult();
+            _client.DisposeAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            var message = JsonSerializer.Deserialize<WeatherRequestModel>(body);
+
+            Console.WriteLine($"Received: {message?.ToString()}");
+
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
     }
 }
